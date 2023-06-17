57ab4e068aa68430a19b1ed176fb8392e776f8ef
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2023-06-17 10:47:17 -0300

    Hrpayroll dinâmico com RestTemplate

--- hrpayroll/src/main/java/br/dev/gabrieldeoliveira/hrpayroll/services/PaymentService.java
+++ hrpayroll/src/main/java/br/dev/gabrieldeoliveira/hrpayroll/services/PaymentService.java
@@ -1,13 +1,33 @@
 package br.dev.gabrieldeoliveira.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.dev.gabrieldeoliveira.hrpayroll.entities.Payment;
+import br.dev.gabrieldeoliveira.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hrworker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Jonh", 235.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", workerId.toString());
+		
+		String url = workerHost + "/workers/{id}"; 
+		
+		Worker worker = restTemplate.getForObject(url, Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
