36fa5bb89344eadb47f792a86639d24fa6cadd31
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2023-06-18 09:38:41 -0300

    Projeto hreurekaserver criado

---
+++ hreurekaserver/src/main/java/br/dev/gabrieldeoliveira/hreurekaserver/HreurekaserverApplication.java
@@ -0,0 +1,13 @@
+package br.dev.gabrieldeoliveira.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HreurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HreurekaserverApplication.class, args);
+	}
+
+}
---
+++ hreurekaserver/src/test/java/br/dev/gabrieldeoliveira/hreurekaserver/HreurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.gabrieldeoliveira.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HreurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
