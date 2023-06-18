32c1a238205ca2138485eb7e024cb5d76e83f490
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2023-06-18 10:02:05 -0300

    Configurando clientes Eureka

--- hrpayroll/src/main/java/br/dev/gabrieldeoliveira/hrpayroll/HrpayrollApplication.java
+++ hrpayroll/src/main/java/br/dev/gabrieldeoliveira/hrpayroll/HrpayrollApplication.java
@@ -2,10 +2,12 @@ package br.dev.gabrieldeoliveira.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HrpayrollApplication {
 
 	public static void main(String[] args) {
