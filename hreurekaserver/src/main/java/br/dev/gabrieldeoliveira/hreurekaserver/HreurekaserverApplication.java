f5db7fba22a000d046cf3cc712c487e965dd8b4c
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2023-06-18 09:44:38 -0300

    Configurando servidor Eureka

--- hreurekaserver/src/main/java/br/dev/gabrieldeoliveira/hreurekaserver/HreurekaserverApplication.java
+++ hreurekaserver/src/main/java/br/dev/gabrieldeoliveira/hreurekaserver/HreurekaserverApplication.java
@@ -2,8 +2,10 @@ package br.dev.gabrieldeoliveira.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HreurekaserverApplication {
 
 	public static void main(String[] args) {
